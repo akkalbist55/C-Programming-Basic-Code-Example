6685f791410b5b78be57643740b9485f05ce5dc3
akkalbist55 <akkalbist55@gmail.com>
2019-03-22 12:43:10 +0545

    first commit in C

---
+++ While_loop.c
@@ -0,0 +1,14 @@
+//While Loop in C Programming Language using gcc codeblock in linux
+#include <stdio.h>
+int main()
+{
+    int num;
+    while(num<=10)
+    {
+        printf("%d\n", num);
+        num++;
+    }
+    return 0;
+}
+
+
---
+++ datatype.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int x, y;
+float salary = 13.48;
+char letter = 'K';
+x = 25;
+y = 34;
+int z = x+y;
+printf("%d \n", z);
+printf("%f \n", salary);
+printf("%c \n", letter);
+return 0;
+}
