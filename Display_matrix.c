b29203f51f65c73e6de868646f16638c9bfb212c
akkalbist55 <akkalbist55@gmail.com>
2019-03-22 16:50:43 +0545

    Third time committeed by AKkal

--- Display_matrix.c
+++ Display_matrix.c
@@ -1,5 +1,6 @@
 //Program to display a matrix on screen
 
+
 #include <stdio.h>
 
 void main()
@@ -46,6 +47,69 @@ printf("\n");
 }
 
 
+
+/*Program to read two M*N matrices and display their sum/difference*/
+
+#include <stdio.h>
+
+#define M 3
+#define N 3
+void main()
+{
+int matrix1[M][N], matrix2[M][N], sum[M][N], i, j;
+
+printf("\nEnter the elements of first matrix:\t");
+for(i=0;i<M;i++)
+{
+for(j=0;j<N;j++)
+{
+scanf("%d", &matrix1[i][j]);
+}
+}
+printf("\nThe first matrix is:\n");
+for(i=0;i<M;i++)
+{
+for(j=0;j<N;j++)
+{
+printf("%d\t", matrix1[i][j]);
+}
+printf("\n");
+}
+
+printf("\nEnter the elements of second matrix:\t");
+for(i=0;i<M;i++)
+{
+for(j=0;j<N;j++)
+{
+scanf("%d",&matrix2[i][j]);
+}
+}
+printf("\nThe second matrix is:\n");
+for(i=0;i<M;i++)
+{
+for(j=0;j<N;j++)
+{
+printf("%d\t",matrix2[i][j]);
+}
+printf("\n");
+}
+for(i=0;i<M;i++)
+{
+for(j=0;j<N;j++)
+{
+sum[i][j]=matrix1[i][j]+matrix2[i][j];
+}
+}
+printf("\nThe sum of the matrices is:\n");
+for(i=0;i<M;i++)
+{
+for(j=0;j<N;j++)
+{
+printf("\t%d", sum[i][j]);
+}
+printf("\n");
+}
+}
 
 
 
