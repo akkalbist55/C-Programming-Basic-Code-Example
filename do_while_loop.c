ac26a6c2408a52935dae5c937c76d1015e1c5039
akkalbist55 <akkalbist55@gmail.com>
2019-03-22 12:49:33 +0545

    commit by akkal

---
+++ do_while_loop.c
@@ -0,0 +1,12 @@
+//do while loop in C Programming Language
+#include <stdio.h>
+int main()
+{
+    int num = 1;
+    do
+    {
+        printf("%d\n", 5*num);
+        num++;
+    }while(num<=100);
+    return 0;
+}
