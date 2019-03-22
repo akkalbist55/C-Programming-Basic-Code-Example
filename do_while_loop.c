9387ccd3280de896b91776e252d37edcbeaa9d9a
akkalbist55 <akkalbist55@gmail.com>
2019-03-22 13:01:27 +0545

    committed by Akkal

--- do_while_loop.c
+++ do_while_loop.c
@@ -1,4 +1,16 @@
 //do while loop in C Programming Language
+
+/*
+A do-while loop is similar to the while loop except that the condition is always executed after the body of a loop. It is also called an exit-controlled loop.
+
+The basic format of while loop is as follows(Syntax):
+
+ do {
+  statements
+} while (expression);
+
+*/
+
 #include <stdio.h>
 int main()
 {
